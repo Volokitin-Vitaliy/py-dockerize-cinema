d19f25ea8e794bf7b8cc0910712680f1146eae53
Fibonacci <volokitin.vitaliy@icloud.com>
2025-10-21 08:21:27 +0300

    Complated

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,10 +137,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/files/media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
